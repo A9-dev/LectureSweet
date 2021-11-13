74e94ea352b972828c11bba60d677e521a82c272
Tom <tom@electrokid.co.uk>
2021-11-13 18:40:46 +0000

    CORS Issue

--- server/server.js
+++ server/server.js
@@ -20,7 +20,12 @@ app.use(sessions({
   resave: false
 }));
 
-app.use(cors());
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/questions"));
