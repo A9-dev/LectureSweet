db64ac1a69ade41e8722bf48062af4efc5f70c50
A9-dev <henrypearson422@gmail.com>
2021-11-14 11:18:09 +0000

    Warning stuff

--- client/src/dashboard/App.js
+++ client/src/dashboard/App.js
@@ -1,20 +1,26 @@
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import VerticalBar from "./VerticalBar.js";
 import ChatList from "./Questions.js";
 
 function App() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
+    <div>
 
-        <VerticalBar />
-      </Grid>
-      <Grid item xs={6}>
 
-        <ChatList />
-      </Grid>
-    </Grid >
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+
+          <VerticalBar />
+
+        </Grid>
+        <Grid item xs={6}>
+
+          <ChatList />
+
+        </Grid>
+
+      </Grid >
+    </div>
 
   );
 }
