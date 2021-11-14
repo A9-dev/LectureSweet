2250b37e5b4231ba3f4f76b965e208bf85f353f8
A9-dev <henrypearson422@gmail.com>
2021-11-14 11:10:36 +0000

    Grid

--- client/src/dashboard/App.js
+++ client/src/dashboard/App.js
@@ -1,15 +1,21 @@
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import VerticalBar from "./VerticalBar.js";
 import ChatList from "./Questions.js";
 
 function App() {
   return (
-      <Container maxWidth="sm">
-        <VerticalBar/>  
-        <br/>
-        <ChatList/>  
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
 
-      </Container>
+        <VerticalBar />
+      </Grid>
+      <Grid item xs={6}>
+
+        <ChatList />
+      </Grid>
+    </Grid >
+
   );
 }
 
