6cb3f0cded622b1d9ee2ec9109e79d3cb6e88409
A9-dev <henrypearson422@gmail.com>
2021-11-14 11:32:49 +0000

    python post

--- final_cam.py
+++ final_cam.py
@@ -1,28 +1,34 @@
 import cv2
 import csv
 import datetime
+import requests
 
 interval = 50
+
 
 def movement(img, total, iteration):
     height = img.shape[0]
     width = img.shape[1]
-    for i in range (0, height):
-        for j in range (0, width):
+    for i in range(0, height):
+        for j in range(0, width):
             pixel = img[i][j]
-            if (pixel != 0): 
+            if (pixel != 0):
                 total += 1
 
-    if(iteration == interval): 
+    if(iteration == interval):
         score = abs(4 * (total - 11100000))
         score = (score / 4260000) * 100
         if (score >= 100):
-            score = 100     
+            score = 100
         total = 0
         iteration = 0
-    
+
         print(score)
-        #do something with score
+        url = "http://localhost:5000/send-camera"
+        myobj = {"cameraValue": score}
+        requests.post(url, myobj)
+
+        # do something with score
 
     iteration += 1
 
@@ -32,40 +38,41 @@ def movement(img, total, iteration):
 startTime = datetime.datetime.now().strftime("%H:%M:%S")
 
 cv2.namedWindow("cam")
-vc = cv2.VideoCapture(0) #opens default camera (could also select a file)
+vc = cv2.VideoCapture(0)  # opens default camera (could also select a file)
 
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():  # try to get the first frame
     rval, firstFrame = vc.read()
 else:
     rval = False
     print("Playback error!")
 
 firstGray = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
-firstGray = cv2.GaussianBlur(firstGray, (5, 5), 0) #to get rid of gaussian noise
+# to get rid of gaussian noise
+firstGray = cv2.GaussianBlur(firstGray, (5, 5), 0)
 
 iteration = 0
-total = 0  
+total = 0
 
 while rval:
-    rval, frame = vc.read() #reads frame
+    rval, frame = vc.read()  # reads frame
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0) #to get rid of gaussian noise
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)  # to get rid of gaussian noise
 
     change = cv2.absdiff(firstGray, gray)
 
     cv2.imshow("cam", change)
     key = cv2.waitKey(20)
-    if key == 27: #exit stream on ESC
+    if key == 27:  # exit stream on ESC
         break
 
-    rval, frame = vc.read() #reads frame
+    rval, frame = vc.read()  # reads frame
 
     firstGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    firstGray = cv2.GaussianBlur(firstGray, (5, 5), 0) 
+    firstGray = cv2.GaussianBlur(firstGray, (5, 5), 0)
 
-    total, iteration= movement(change, total, iteration)
-    #do something with score ^^
+    total, iteration = movement(change, total, iteration)
+    # do something with score ^^
 
-vc.release() #cleans up
+vc.release()  # cleans up
 cv2.destroyWindow("cam")
